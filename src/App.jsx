3b7d1feebe658538159b4a70a661cf844177dba9
karim <abdelkarimkribi@email.com>
2025-08-07 00:22:21 +0100

    test

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Home = () => <h1>Accueil</h1>;
+const Menu = () => <h1>Menu</h1>;
+const Concept = () => <h1>Concept</h1>;
+const Gallery = () => <h1>Galerie</h1>;
+const Contact = () => <h1>Contact</h1>;
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/concept" element={<Concept />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );A
+}
+
+export default App;
